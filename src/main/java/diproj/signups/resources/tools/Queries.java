e1fbec520da99db14196d08d3d0897a18284558d
D-Hanraads <56079430+D-Hanraads@users.noreply.github.com>
2021-06-27 21:39:42 +0200

    Added query that adds manager, municipality and 10 employees, added file with login codes removed old javaclass

--- src/main/java/diproj/signups/resources/tools/Queries.java
+++ src/main/java/diproj/signups/resources/tools/Queries.java
@@ -33,7 +33,7 @@ public class Queries {
         query6 = "INSERT INTO people (person_id, name ,password, phone_number, email, role)\n" +
                 "VALUES('" + i + "', '" + name + "', '" + hash.hashPassword(password) + "', '" + number + "', '" + email + "', '" + role + "')";
         if (role.equals("Field Engineer")) {
-            query6 = query6 + "\n INSERT INTO employee(person_id, working_hours, last_active, area) \n" +
+            query6 = query6 + ";\n INSERT INTO employee(person_id, working_hours, last_active, area) \n" +
                     "VALUES ('"  + i + "', '" + hours + "', '" + active + "', '" + area + "')";
         }
         return query6;
@@ -50,9 +50,31 @@ public class Queries {
             Connection connection = DriverManager.getConnection(url, dbName, "sH9CxfLuJtu60On2");
 
 
-            String sql = FillDB("100","Peter", "Hallo123", "0692964771", "Peter@manager.com", "Manager", "", "", "");
+            String sql1 = FillDB("1","Peter Post", "Hallo123", "0692964771", "Peter@manager.com", "Manager", "", "", "");
+            String sql2 = FillDB("2","Kees Jansma", "Cheese123", "0692964772", "Kees@municipality.com", "Municipality", "", "", "");
+            String sql3 = FillDB("3","Amalia Bloemendaal", "Sausage123", "0692964773", "Amalia@employee.com", "Field Engineer", "9:00-17:00", "2021-06-10", "North");
+            String sql4 = FillDB("4","Hagrid Grootmeester", "Wizard123", "0692964774", "Hagrid@employee.com", "Field Engineer", "9:00-17:00", "2021-05-16", "South");
+            String sql5 = FillDB("5","Leo Blok", "grybb123", "0692964775", "Leo@employee.com", "Field Engineer", "9:00-17:00", "2021-05-19", "Mid");
+            String sql6 = FillDB("6","Daan Hanraads", "toto123", "0692964776", "Daan@employee.com", "Field Engineer", "9:00-17:00", "2021-06-15", "West");
+            String sql7 = FillDB("7","Dirk can de Ruit", "orange123", "0692964777", "Dirk@employee.com", "Field Engineer", "9:00-17:00", "2021-07-10", "East");
+            String sql8 = FillDB("8","Diana Bozea", "Hello123", "0692964778", "Diana@employee.com", "Field Engineer", "10:00-19:00", "2021-05-19", "North");
+            String sql9 = FillDB("9","Edina Depner", "Cheers123", "0692964779", "Edina@employee.com", "Field Engineer", "10:00-19:00", "2021-05-16", "South");
+            String sql10 = FillDB("10","Rob Brommer", "Assist123", "0692964710", "Rob@employee.com", "Field Engineer", "10:00-19:00", "2021-05-13", "East");
+            String sql11 = FillDB("11","Wouter Bos", "Goalsq123", "0692964711", "Wouter@employee.com", "Field Engineer", "10:00-19:00", "2021-05-12", "West");
+            String sql12 = FillDB("12","Willem de Koning", "Crown123", "0692964712", "Willem@employee.com", "Field Engineer", "10:00-19:00", "2021-05-10", "Mid");
             Statement statement = connection.createStatement();
-            statement.executeUpdate(sql);
+            statement.executeUpdate(sql1);
+            statement.executeUpdate(sql2);
+            statement.executeUpdate(sql3);
+            statement.executeUpdate(sql4);
+            statement.executeUpdate(sql5);
+            statement.executeUpdate(sql6);
+            statement.executeUpdate(sql7);
+            statement.executeUpdate(sql8);
+            statement.executeUpdate(sql9);
+            statement.executeUpdate(sql10);
+            statement.executeUpdate(sql11);
+            statement.executeUpdate(sql12);
             connection.close();
 
 
